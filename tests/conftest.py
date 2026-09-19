5fc6578917c6ab633b8eb343c1cbd0c30c066b7f
CH4rnel <frankmozes777lucky@gmail.com>
2026-09-19 15:57:24 +0300

    add src directory to Python path for test discovery

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+# inb4: just_for_lulz
+
+import sys
+import os
+
+# Add src directory to Python path for test discovery
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
